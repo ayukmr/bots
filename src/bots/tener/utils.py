5ae02f311f71d4ec672b4560e216e73b57c5c1af
Ayush Kumar <github@ayukmr.com>
2026-01-03 21:50:32 -0800

    Formatting

--- src/bots/tener/utils.py
+++ src/bots/tener/utils.py
@@ -13,25 +13,19 @@ async def enhance(message: discord.Message) -> None:
     attachment = message.attachments[0]
     file = BytesIO(await attachment.read())
 
-    if (
-        any(map(
-            lambda start: message.content.startswith(start),
-            ['renet', 'evah ot']
-        )) or
-        any(map(
-            lambda end: message.content.endswith(end),
-            ['renet', 'evah ot']
-        ))
-    ):
+    if any(map(
+        lambda s: message.content.startswith(s) or message.content.endswith(s),
+        ['renet', 'evah ot']
+    )):
         orig = Image.open(file)
 
         image = ImageOps.invert(orig.convert('RGB'))
 
         enhancer = ImageEnhance.Contrast(image)
-        image    = enhancer.enhance(2)
+        image = enhancer.enhance(2)
 
         enhancer = ImageEnhance.Color(image)
-        image    = enhancer.enhance(2)
+        image = enhancer.enhance(2)
 
         image = image.filter(ImageFilter.SHARPEN)
 
